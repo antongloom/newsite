7fc22e64a1624821ee846d36f4edac774b734283
Anton <antongloom89@gmail.com>
2019-08-25 21:23:26 +0300

    Правки по layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,35 @@
 <template>
 	<div class="MainLayout">
-		<router-view />
+		<div class="MainLayout-Content">
+			<Navbar />
+			<div class="MainLayout-Item">
+				<Sidebar />
+				<router-view />	
+			</div>
+		</div>
 	</div>
 </template>
-<script>
-	
-</script>
-<style lang="stylus" scoped>
 
+
+<style lang="stylus" scoped>
+	.MainLayout
+		&-Content
+			max-width 1920px
+			margin 0 auto
+		&-Item
+			display flex
+			justify-content space-between
+			padding-top 67px
 </style>
+
+<script>
+import Navbar from '@/components/app/Navbar'
+import Sidebar from '@/components/app/Sidebar'
+export default {
+  name: 'main-layout',
+  components: {
+		Navbar,
+		Sidebar
+	}
+}	
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,40 @@
 <template>
   <div class="Home">
-		<div class="Home-Content">
-	    <Navbar />
-	    <div class="Home-Item">
-	    	<Sidebar />
-	    	<div class="Home-Info">
-	    		<div class="Home-Title">
-	    			<div class="Home-Text">Счет</div>
-	    			<div class="Home-Update">&#8634;</div>
-	    		</div>
-	    		<div class="Home-InfoBlock">
-	    			<div class="Home-Price">
-	    				<div class="Home-Subtitle">Счет в валюте</div>
-	    				<div class="Home-PriceNumber">12.0 <span>Р</span></div>
-	    			</div>
-	    			<div class="Home-Price Home-Price_Left">
-	    				<div class="Home-Subtitle">Курс валют</div>
-	    				<div class="Home-PriceText Home-PriceText_Title">
-	    					<ul>
-	    						<li>Валюта</li>
-	    						<li>Курс</li>
-	    						<li>Дата</li>
-	    					</ul>
-	    				</div>
-	    				<div class="Home-PriceText">
-	    					<ul>
-	    						<li>руб</li>
-	    						<li>12121</li>
-	    						<li>12.12.12</li>
-	    					</ul>
-	    				</div>
-	    			</div>
-	    		</div>
-	    	</div>
-	    </div>
-    </div>
+  		<div class="Home-Title">
+  			<div class="Home-Text">Счет</div>
+  			<div class="Home-Update">&#8634;</div>
+  		</div>
+  		<div class="Home-InfoBlock">
+  			<div class="Home-Price">
+  				<div class="Home-Subtitle">Счет в валюте</div>
+  				<div class="Home-PriceNumber">12.0 <span>Р</span></div>
+  			</div>
+  			<div class="Home-Price Home-Price_Left">
+  				<div class="Home-Subtitle">Курс валют</div>
+  				<div class="Home-PriceText Home-PriceText_Title">
+  					<ul>
+  						<li>Валюта</li>
+  						<li>Курс</li>
+  						<li>Дата</li>
+  					</ul>
+  				</div>
+  				<div class="Home-PriceText">
+  					<ul>
+  						<li>руб</li>
+  						<li>12121</li>
+  						<li>12.12.12</li>
+  					</ul>
+  				</div>
+  			</div>
+  		</div>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
 export default {
   name: 'home',
   components: {
-		Navbar,
 		Sidebar
 	}
 }
@@ -52,36 +42,8 @@ export default {
 
 <style lang="stylus" scoped>
 	.Home
-		&-Content
-			max-width 1920px
-			margin 0 auto
-		&-Header
-			padding 15px
-			display flex
-			justify-content space-between
-			align-items center
-			background #FF8C00
-			-webkit-box-shadow 0px 2px 4px 1px rgba(0,0,0,0.32)
-			-moz-box-shadow:0px 2px 4px 1px rgba(0,0,0,0.32)
-			box-shadow 0px 2px 4px 1px rgba(0,0,0,0.32)
-			position fixed
-			left 0px
-			right 0px
-		&-Btn
-			display flex
-			align-items center
-		&-Img
-			cursor pointer
-		&-Time
-			margin-left 15px
-		&-User
-			text-transform uppercase
-			img
-				margin-left 5px
-		&-Item
-			display flex
-			justify-content space-between
-			padding-top 67px
+		width 100%
+		padding 30px 15px 0px 15px
 		&-Title
 			display flex
 			justify-content space-between
@@ -100,9 +62,6 @@ export default {
 			text-align center
 			line-height 30px
 			cursor pointer
-		&-Info
-			width 100%
-			padding 30px 15px 0px 15px
 		&-InfoBlock
 			display flex
 			justify-content space-between
